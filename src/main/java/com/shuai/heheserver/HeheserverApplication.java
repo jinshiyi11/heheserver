83e127473889739f9b0885db3fae053b1f4874c1
jinshiyi11 <49662990@qq.com>
2017-12-14 17:45:59 +0800

    1.init

---
+++ src/main/java/com/shuai/heheserver/HeheserverApplication.java
@@ -0,0 +1,12 @@
+package com.shuai.heheserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeheserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeheserverApplication.class, args);
+	}
+}
